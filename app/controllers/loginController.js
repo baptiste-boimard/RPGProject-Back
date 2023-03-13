6ce94aca5557f45560419a7b8e13258f0e9f5600
baptiste-boimard <bouketin27@gmail.com>
2023-03-13 14:33:53 +0100

    Add method signup to logincontroller

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -5,6 +5,8 @@
  * @namespace adminController
  */
 const loginController =  {
+
+
   /** The method allows you to log as an user
    * @memberof loginController
    * @method login
@@ -13,6 +15,25 @@ const loginController =  {
    * @returns {Object} Return response to login
    */
   async login(req,res,next) {
+    console.log(req.body);
+    if(req.body.email == 'dd@dd' && req.body.password == 'dd') {
+      const user = 'coucou';
+      res.json({user});
+    } else {
+      const err = new Error('Mauvais truc')
+      next(err);
+    }
+  },
+
+  /** The method allows you to signup a new user
+   * @memberof loginController
+   * @method signup
+   * @param {Object} req.body
+   * @param {Object} routers
+   * @returns {Object} Return response to signup
+   */
+  async signup(req,res,next) {
+    console.log(req.body);
     if(req.body.email == 'dd@dd' && req.body.password == 'dd') {
       const user = 'coucou';
       res.json({user});
--- app/routers/router.js
+++ app/routers/router.js
@@ -6,5 +6,7 @@ const routerWrapper = require('../handlers/routerWrapper');
 const loginController = require('../controllers/loginController');
 
 router.post('/login', routerWrapper(loginController.login));
+router.post('/signup', routerWrapper(loginController.signup));
+
 
 module.exports = router;
